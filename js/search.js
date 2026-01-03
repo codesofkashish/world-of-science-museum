344946217304042a5e80b83ea41b24a43fdb6b36
Kashish Upadhyay <kashishups@gmail.com>
2026-01-04 01:39:32 +0530

    Update search.js

--- js/search.js
+++ js/search.js
@@ -8,7 +8,7 @@ function searchSite() {
 
   // pages & keywords
   const pages = {
-    "home": "/index.html",
+    "home": "../index.html",
     "history": "../html/history.html",
     "about": "../html/about.html",
     "gallery": "../html/gallery.html",
@@ -26,4 +26,5 @@ function searchSite() {
 
   alert("No results found 😔");
 }
+
 
