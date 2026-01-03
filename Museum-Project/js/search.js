a4310df96e1968379362d735c6266be799b4a1ce
Kashish Upadhyay <kashishups@gmail.com>
2026-01-03 19:16:34 +0530

    Add files

---
+++ Museum-Project/js/search.js
@@ -0,0 +1,28 @@
+function searchSite() {
+  const query = document.getElementById("searchInput").value.toLowerCase().trim();
+
+  if (query === "") {
+    alert("Please enter something to search");
+    return;
+  }
+
+  // pages & keywords
+  const pages = {
+    "home": "../html/index.html",
+    "history": "../html/history.html",
+    "about": "../html/about.html",
+    "gallery": "../html/gallery.html",
+    "contact": "../html/contact.html",
+    "science": "../html/science.html",
+    "museum": "../html/index.html"
+  };
+
+  for (let key in pages) {
+    if (query.includes(key)) {
+      window.location.href = pages[key];
+      return;
+    }
+  }
+
+  alert("No results found 😔");
+}
